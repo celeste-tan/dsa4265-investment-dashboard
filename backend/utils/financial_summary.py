ba7fce22ca9f2f2b48189be30ab01199c595d3a0
Nicole <nicole.chong2002@gmail.com>
2025-04-16 17:06:18 +0800

    clean media file, and evaluation functions for the 4 dimensions

--- backend/utils/financial_summary.py
+++ backend/utils/financial_summary.py
@@ -242,6 +242,9 @@ def evaluate_financial_commentary_faithfulness(tickers, openai_api_key, period="
     #print(f"\nFaithfulness evaluations saved to: {filepath}")
     return all_results
 
-# # Example usage
+# -----------------------------------------------------------------------------------------
+# Evaluation Testing (Commented out by default, only run when evaluation needs to be done)
+# -----------------------------------------------------------------------------------------
 # tickers_to_check = ["TSLA", "NVDA", "AAPL", "MSFT", "GOOGL ", "META", "AMZN", "PLTR ", "AMD", "NFLX"]
-# evaluate_financial_commentary_faithfulness(tickers_to_check, "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")
+# openai_api_key = os.getenv("OPENAI_API_KEY")
+# evaluate_financial_commentary_faithfulness(tickers_to_check, openai_api_key)
